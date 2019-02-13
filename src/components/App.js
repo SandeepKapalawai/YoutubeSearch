033ac36c867ec48477508c7fad562a0530e532ef
Sandeep Kapalawai <kapalawai.sandeep.sandeep@gmail.com>
2019-02-13 11:35:06 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
 
 
 
 class App extends React.Component{
 
     state ={ 
-        videos : []
+        videos : [],
+        selectedVideo: null
     };
+
+    componentDidMount(){
+        this.onTextSubmit('buildings');
+    }
 
     onTextSubmit =  async text => {
         const response =  await youtube.get('/search',{
@@ -19,16 +26,32 @@ class App extends React.Component{
         }
         });
         console.log(response.data.items)
-        this.setState ( {videos : response.data.items})
+        this.setState ( {videos : response.data.items,
+            selectedVideo:response.data.items[0]
+        })
     };
+
+    onVideoSelect= (video) => {
+      console.log('from the app', video);
+      this.setState({selectedVideo : video});
+    }
 
     render()
     {
         return(
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onFormSubmit={this.onTextSubmit}/>
-                <VideoList videos={this.state.videos} />
-                i have {this.state.videos.length} videos
+               <div className = "ui grid">
+                <div className="ui row">
+                <div className="eleven wide column">
+                <VideoDetail video ={this.state.selectedVideo} />
+                </div>
+               <div className="five wide column">
+               <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+               </div>
+                </div>
+               </div>
+   
             </div>
         )
     }
